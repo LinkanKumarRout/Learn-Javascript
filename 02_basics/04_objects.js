603fbe550caf5edb535b0542062b7358e18880f0
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-02-10 09:39:41 +0000

    learn Object de-structuring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -34,9 +34,36 @@ const obj2 = {
 const obj3 = {...obj1, ...obj2};
 // console.log(obj3);
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('name'));
+// console.log(tinderUser.hasOwnProperty('name'));
+
+const course = {
+    coursename: "JS in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+};
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course;
+
+console.log(instructor);
+
+// JSON from API
+// {
+//     "name": "Linkan Kumar Rout",
+//     "course": "JS in hindi",
+//     "fees": "free"
+// }
+
+// or
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
