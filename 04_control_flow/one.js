abab907593ed583493e0ad8be544d9b03df932bc
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-02-16 05:12:58 +0000

    learn control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,24 @@
+// if statement
+
+let isUserLoggedIn = true;
+if(isUserLoggedIn){
+    console.log("Logging in....");
+}
+
+// all comparision operator
+/*
+    <, >, <=, >=, ==, !=
+*/
+
+if (2 == "2"){
+    console.log("executed");
+}
+
+// if-else statement
+
+let temperature = 41;
+if (temperature < 50){
+    console.log(`Temperature is less than 50`);
+} else {
+    console.log(`Temperature is greater than 50`);
+}
