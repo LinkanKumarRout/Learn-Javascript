c46a35f51d0b90cd3220d48864bcea501640ba03
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-02-16 09:45:24 +0000

    learn for loop

---
+++ 05_loops/for loop.js
@@ -0,0 +1,14 @@
+// for loop
+
+let array = [10, 20, 30, 40, 50];
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element);
+}
+
+
+
+
+
+
+
