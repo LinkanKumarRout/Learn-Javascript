c9d326a2d1fabf1bcf28a4ef61502217641d2071
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-02-17 16:03:59 +0000

    learn more about for loop

--- 05_loops/for loop.js
+++ 05_loops/for loop.js
@@ -1,8 +1,27 @@
 // for loop
 
-let array = [10, 20, 30, 40, 50];
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
+    if(element === 5){
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+}
+// console.log(element); // error
+
+for(let i = 1; i <= 10; i++){
+    // console.log(`Outer loop value is ${i}`);
+    for(let j = 1; j <= 10; j++){
+        // console.log(`Inner loop value is ${j} and outer loop is ${i}`);
+        // console.log(`${i} * ${j} = ${i*j}`);
+    }
+}
+
+let arr = ["flash", "batman", "superman"]
+console.log(arr.length)
+for(let index = 0; index < arr.length; index++){
+    const element = arr[index];
     console.log(element);
 }
 
