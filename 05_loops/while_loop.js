a2a5806f77ec7a9531ef99cda5f953ccec46b7d4
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-02-25 14:48:46 +0000

    learn while loop in JS

---
+++ 05_loops/while_loop.js
@@ -0,0 +1,22 @@
+// while loop
+
+let i = 1;
+while(i <= 10){
+    // console.log(`Value of i is ${i}`);
+    i = i + 1
+}
+
+let myArray = ['flash', 'batman', 'superman'];
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr ++;
+}
+
+// do-while loop
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
