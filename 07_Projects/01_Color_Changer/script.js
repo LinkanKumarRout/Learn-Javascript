0db6cd1dc55ce839ad4381a6fa2ee52d40369196
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-02-28 15:50:48 +0000

    project 1 complete

--- 07_Projects/01_Color_Changer/script.js
+++ 07_Projects/01_Color_Changer/script.js
@@ -5,7 +5,22 @@ const body = document.querySelector('body');
 
 buttons.forEach(function(button){
     button.addEventListener('click', function(event){
-        console.log(event);
-        console.log(event.target);
+        if(event.target.id === 'violet'){
+            body.style.backgroundColor = event.target.id;
+        }else if(event.target.id === 'indigo'){
+            body.style.backgroundColor = event.target.id;
+        }else if(event.target.id === 'blue'){
+            body.style.backgroundColor = event.target.id;
+        }else if(event.target.id === 'green'){
+            body.style.backgroundColor = event.target.id;
+        }else if(event.target.id === 'yellow'){
+            body.style.backgroundColor = event.target.id;
+        }else if(event.target.id === 'orange'){
+            body.style.backgroundColor = event.target.id;
+        }else if(event.target.id === 'red'){
+            body.style.backgroundColor = event.target.id;
+        }else if(event.target.id === 'white'){
+            body.style.backgroundColor = event.target.id;
+        }
     })
-})
+});
