3de622cd643fbc7958e2d2f366b357010df77f9a
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-02-28 08:33:08 +0000

    created files for color changer project

---
+++ 07_Projects/01_Color_Changer/script.js
@@ -0,0 +1,11 @@
+// color changer Script
+
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector('body');
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(event){
+        console.log(event);
+        console.log(event.target);
+    })
+})
