1af5bd0c4fac7b075dee67f29e45ae76f16e87c7
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-03-04 14:37:56 +0000

    Project 6 completed

---
+++ 07_Projects/06_Unlimited_Colors/script.js
@@ -0,0 +1,28 @@
+// Unlimited Colors
+
+// generate a random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+
+let intervalId;
+
+let startChangingColor = function(){
+    intervalId = setInterval(changingColor, 1000);
+    function changingColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+let stopChangingColor = function(){
+    clearInterval(intervalId);
+}
+
+document.getElementById('start').addEventListener('click', startChangingColor)
+document.getElementById('stop').addEventListener('click', stopChangingColor);
