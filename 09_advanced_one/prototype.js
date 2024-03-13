b1744ad69408933d94ff9cbd7d8deb338f5009a1
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-03-13 03:02:47 +0000

    learn how new keyword works

--- 09_advanced_one/prototype.js
+++ 09_advanced_one/prototype.js
@@ -18,3 +18,16 @@ function createUser(username, score){
     this.username = username;
     this.score = score;
 }
+
+createUser.prototype.increment = function(){
+    this.score++ // this refers to the current context
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`Score is ${this.score}`)
+}
+
+const chai = new createUser("Chai", 25) // if you don't put new keyword this will not work
+const tea = new createUser("tea", 250)
+
+chai.printMe();
