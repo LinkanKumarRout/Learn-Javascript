1b6771cb4d1b51f460998bde0115ebfebbabaf12
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-03-15 08:52:25 +0000

    learn getter and setter in JS

---
+++ 10_class_&_oops/getter_setter.js
@@ -0,0 +1,21 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    }
+    // getter and setter name remains same as constructor arguments
+
+    // here we give _password bcz of naming conflict between this.password of constructor and this._password of get and set
+
+    get password(){ // this will take value from out of class
+        return this._password.toUpperCase()
+    }
+
+    set password(value){ // this will change the value but original value remains same
+        this._password = value; // never use return in setter
+    }
+}
+
+const linkan = new User('link@google.com', '123abc');
+
+console.log(linkan.password);
