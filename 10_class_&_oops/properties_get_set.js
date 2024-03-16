d65f4c9b4df02b5b29717e21a41ade8780a00ba9
Linkan Kumar Rout <87231777+LinkanKumarRout@users.noreply.github.com>
2024-03-16 08:05:39 +0000

    old style of creating getter and setter ( function )

---
+++ 10_class_&_oops/properties_get_set.js
@@ -0,0 +1,29 @@
+// Old style of using getter and setter
+
+function User(email, password){
+    this._email = email
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+}
+
+const linkan = new User('linkan@google.com', '123abc');
+
+console.log(linkan.email);
+console.log(linkan.password);
